d2c72a1d00cf9b24e7a3d75593bd8cdcea30211e
Linh Chi Nguyen <linhchinguyen@gmx.de>
2020-06-15 23:35:40 +0200

    new update

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
@@ -44,3 +44,4 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
