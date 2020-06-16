e6bc19ae1aeb2d2ed9f68b11502f2a86ae8330f2
Linh Chi Nguyen <linhchinguyen@gmx.de>
2020-06-16 04:15:01 +0200

    eigenschaften

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,22 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+choices = Category.objects.all().values_list('name', 'name')
+
+choice_list = []
+
+for item in choices:
+    choice_list.append(item)
 
 class PostForm(forms.ModelForm):
 
     class Meta:
          model = Post
-         fields = ('title', 'text',)
+         fields = ('title', 'text','category','country')
+
+         widgets = {
+             'title': forms.TextInput(attrs={'class': 'form-control'}),
+             'text': forms.Textarea(attrs={'class': 'form-control'}),
+             'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+             'country': forms.TextInput(attrs={'class': 'form-control'}),
+         }
--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,22 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    category = models.CharField(max_length=200, default ="uncategorized")
+    country = models.CharField(max_length=200, default ="uncategorized")
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
