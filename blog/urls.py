1bf9528906ed5037ac68bdc60534833be877fe5e
Linh Chi Nguyen <linhchinguyen@gmx.de>
2020-07-13 10:19:59 +0200

    about html als extended base html, verlinkung html

--- blog/urls.py
+++ blog/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/<pk>/remove/', views.post_remove, name='post_remove'),
     path('about.html',views.about, name='about'),
-    path('abouts', views.abouts_detail, name='abouts_detail'),
     path('checklist.html',views.todolist, name='checklist'),
     path('todo_detail/<int:pk>/', views.todo_detail, name='todo_detail'),
     path('delete/<int:pk>/', views.deleteTask, name='delete'),
